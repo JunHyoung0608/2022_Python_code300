74815d61a119799db866d12028e677e35b3602dd
JunHyoung0608 <wnsgud1235686@gmail.com>
2022-09-15 14:33:23 +0900

    Python

---
+++ aa.py
@@ -0,0 +1,4 @@
+for i in range(1,301):
+    code="code300/problem{0:0>3}.py".format(i)
+    with open(code,"w") as file:
+        pass
---
+++ bb.py
@@ -0,0 +1,6 @@
+a = 1
+
+st = '{0:0>3}'.format(a)
+
+
+print(st)
